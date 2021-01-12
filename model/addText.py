9453a4e630d76035385f4f0d85f428d5a965104d
meytalmesh <meytalmesh@gmail.com>
2021-01-12 17:51:59 +0200

    comit

---
+++ model/addText.py
@@ -0,0 +1,15 @@
+import os
+import sqlite3
+from model import TextConcrete
+
+
+def addtext(folderName):
+    for file in os.listdir(folderName):
+        fname= folderName+"/"+file
+        f = open(fname, "r")
+        text= f.read()
+        TextConcrete.insert_text(None, text)
+
+
+
+addtext("C:/Users/meital/Desktop/פרוייקט/DUC/DUC_2002/texts/d061j")
